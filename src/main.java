4c3d1c34d010816a7a62b59ecaf4caaf3f1c8572
unknown <mkmedia92@hotmail.com>
2019-09-26 19:27:25 -0400

    update

--- src/main.java
+++ src/main.java
@@ -2,13 +2,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String teams[] = {"Hawks","Tigers","Bills","Lions","Eagles",
-						"Runners","Kills","Dolphins","Diamonds","Rings","Orange",
-						"Gold","Jaguar",};
-       
-        Schedule.ScheduleFinder(teams);
-        
-        //Combo.count();
+		String teams[] = { "Hawks", "Tigers", "Bills", "Lions", "Eagles", "Runners", "Kills", "Dolphins", "Diamonds",
+				"Rings", "Orange", "Jets", "Gold", "Jaguar", };
+
+		Schedule.ScheduleFinder(teams);
+
+		// Combo.count();
 
 	}
 
